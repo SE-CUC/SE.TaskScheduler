5581f6731f8942775f93c72e10bb29d779f1d77d
k-edr <yevhenpuzenko@gmail.com>
2025-08-28 14:02:38 +0200

    added extra constructors

--- SE.TaskScheduler.Mixin/Core/Models/MyTask.cs
+++ SE.TaskScheduler.Mixin/Core/Models/MyTask.cs
@@ -9,5 +9,21 @@ namespace IngameScript
         public TaskType Type { get; set; }
         public IEnumerator Task { get; set; }
         public TaskPriority Priority { get; set; } = TaskPriority.Normal;
+
+        public MyTask() { }
+
+        public MyTask(TaskType type, IEnumerator task, TaskPriority priority = TaskPriority.Normal)
+        {
+            Type = type;
+            Task = task;
+            Priority = priority;
+        }
+
+        public MyTask(TaskType type, IEnumerable task, TaskPriority priority = TaskPriority.Normal)
+        {
+            Type = type;
+            Task = task.GetEnumerator();
+            Priority = priority;
+        }
     }
 }
